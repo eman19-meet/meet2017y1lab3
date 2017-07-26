2ef3205f8c70e631ab7b430c1f17501d67492163
eman19-meet <eman19@meet.mit.edu>
2017-07-26 18:25:19 +0300

    Stopping here

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>for number=>1 or number=<100:
+    if number%3==0:
+        print('Fizz!')
+    if number%5==0:
+        print('Buzz!')
+    if number%3==0 and number%5==0:
+        print('FizzBuzz!')
+#THIS IS NOT CORRRECT!
